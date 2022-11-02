504caa68495380a3e1995ff54ea149c84f913c3f
jesinthan0703 <jesinthan.23cs@licet.ac.in>
2022-11-02 22:32:29 +0530

    Assignment 2 added

--- Assignments/Jesinthan/Assignment2/app.py
+++ Assignments/Jesinthan/Assignment2/app.py
@@ -1,50 +1,71 @@
-from flask import Flask
-from flask import render_template
+from flask import  Flask,render_template, request, redirect, url_for, session
+import ibm_db
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('home.html')
+# Database Connection
+try:
+    conn = ibm_db.connect("DATABASE=BLUDB;HOSTNAME=fbd88901-ebdb-4a4f-a32e-9822b9fb237b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32731;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=tfy84094;PWD=9ibreJkzJF8l0ZTA",'','')
+    print("Connected to database: ", conn)
+except:
+    print ("Unable to connect to the database")
 
-
-@app.route('/sign-in')
-def signin_form():
-    return render_template('signin.html')
-
-
-@app.route('/signup')
-def signup_form():
-    return render_template('signup.html')
-
-
-@app.route('/insertUser', methods=['POST', 'GET'])
-def addrec():
+@app.route('/',methods = ['POST', 'GET'])
+def home():
     if request.method == 'POST':
-        try:
-            name = request.form['email']
-            password = request.form['password']
 
-            with sql.connect("student_database.db") as con:
-                cur = con.cursor()
-                cur.execute(
-                    "INSERT INTO users (email,password) VALUES (?,?)", (email, password))
-                con.commit()
-                msg = "Record successfully added!"
-        except:
-            con.rollback()
-            msg = "error in insert operation"
+        username = request.form['username']
+        rollnumber = request.form['rollnumber']
+        email = request.form['email']
+        password = request.form['password']
+        print(username,rollnumber,email,password)
 
-        finally:
-            return render_template("home.html")
-            con.close()
+        sql = "SELECT * FROM user WHERE username = '"+username+"' "
+        print(sql)
+        stmt = ibm_db.exec_immediate(conn, sql)
+        
+        account = ibm_db.fetch_assoc(stmt)
+        print(account)
 
+        if account:
+            print("User already exists")
+            return render_template('register.html')
+        else:
+            print("User does not exist")
+            insert_query = "INSERT INTO user VALUES('"+username+"','"+email+"','"+password+"','"+rollnumber+"')"
+            ibm_db.exec_immediate(conn, insert_query)
+            print("You are successfully registered")
+            return render_template('login.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+    return render_template('register.html')
 
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+    print("Login")
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+      
+        print(email,password)
 
-if __name__ == "__main__":
+        sql = "SELECT password FROM user WHERE email = '"+email+"' "
+        print(sql)
+        stmt = ibm_db.exec_immediate(conn, sql)
+        print(stmt)
+        pwd = ibm_db.fetch_assoc(stmt)
+        print(pwd)
+        key = pwd.get('PASSWORD')
+
+        if password==key.rstrip():
+            print("User exists")
+            return render_template('welcome.html')
+        else:
+            print("User does not exist")
+            return render_template('register.html')
+
+    return render_template('login.html')
+
+if __name__ == "main":
     app.run(debug=True)
+    
